ab5212e527e5cfedd245cef3cd6d33f2632490fc
vrdons <vrdons@proton.me>
2026-09-13 12:21:38 +0300

    chore(legacy): Drop legacy support completely
    We will add that feature later

--- crates/legacy/src/protocol/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod identifier;
-pub mod packets;
-
-pub use identifier::{BedrockIdentifier, IdentifierError};
-pub use mineshaft_server::protocol::{HeaderError, MineshaftHeader, MINESHAFT_HEADER_LEN};
-pub use packets::*;
